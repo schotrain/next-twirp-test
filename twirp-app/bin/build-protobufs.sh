58589b2f13b106267125774d696213290943b532
Nick <nschofield@gmail.com>
2021-01-10 22:18:38 -0500

    Script update

--- twirp-app/bin/build-protobufs.sh
+++ twirp-app/bin/build-protobufs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rotoc --plugin=$GOPATH/bin/protoc-gen-twirpy --python_out=./generated --twirpy_out=./generated --proto_path=../protobufs ../protobufs/*.proto
+protoc --plugin=$GOPATH/bin/protoc-gen-twirpy --python_out=./generated --twirpy_out=./generated --proto_path=../protobufs ../protobufs/*.proto
